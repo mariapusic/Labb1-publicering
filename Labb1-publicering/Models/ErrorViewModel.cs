a3e715cf7d95fcb329ae92aa4867b517c6c40cbc
Maria Pusic <maria.pusic@iths.se>
2023-06-07 15:56:22 +0200

    Add project files.

---
+++ Labb1-publicering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Labb1_publicering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
